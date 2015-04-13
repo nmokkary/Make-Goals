6669fdfbc0cccad8c0b2453335251f2a49b60699
Naser Mokkary <nmokkary@gmail.com>
2015-04-13 15:48:23 +0430

    Initial Commit

---
+++ Make Goals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Make Goals
+//
+//  Created by Naser Mokkary on 4/13/15.
+//  Copyright (c) 2015 nmokkary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
